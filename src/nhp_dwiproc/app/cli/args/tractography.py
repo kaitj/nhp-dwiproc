6be256397e1b6df4e5b319b7e3cadb387bb2bdb4
Jason Kai <jason.kai@childmind.org>
2024-10-15 13:24:42 -0400

    Init breakdown of tractography args for act

--- src/nhp_dwiproc/app/cli/args/tractography.py
+++ src/nhp_dwiproc/app/cli/args/tractography.py
@@ -1,21 +1,28 @@
 """Sub-module containing tractography optional arguments."""
 
+from argparse import _ArgumentGroup
+
 from bidsapp_helper.parser import BidsAppArgumentParser
 
 
 def add_tractography_args(app_parser: BidsAppArgumentParser) -> None:
     """Tractography analysis-level arguments."""
-    tractography_args = app_parser.add_argument_group(
+    arg_group = app_parser.add_argument_group(
         "tractography analysis-level options",
     )
-    tractography_args.add_argument(
+    _add_general(arg_group)
+
+
+def _add_general(arg_group: _ArgumentGroup) -> None:
+    """General tractography arguments."""
+    arg_group.add_argument(
         "--single-shell",
         "--single_shell",
         dest="participant.tractography.single_shell",
         action="store_true",
         help="process single-shell data (default: %(default)s)",
     )
-    tractography_args.add_argument(
+    arg_group.add_argument(
         "--shells",
         metavar="shell",
         dest="participant.tractography.shells",
@@ -23,7 +30,7 @@ def add_tractography_args(app_parser: BidsAppArgumentParser) -> None:
         type=int,
         help="space-separated list of b-values (b=0 must be included explicitly)",
     )
-    tractography_args.add_argument(
+    arg_group.add_argument(
         "--lmax",
         metavar="lmax",
         dest="participant.tractography.lmax",
@@ -33,14 +40,14 @@ def add_tractography_args(app_parser: BidsAppArgumentParser) -> None:
         (space-separated for multiple b-values, b=0 must be included explicitly)
         """,
     )
-    tractography_args.add_argument(
+    arg_group.add_argument(
         "--steps",
         metavar="steps",
         dest="participant.tractography.steps",
         type=float,
         help="step size (in mm) for tractography (default: 0.5 x voxel size)",
     )
-    tractography_args.add_argument(
+    arg_group.add_argument(
         "--tractography-method",
         "--tractography_method",
         metavar="method",
@@ -50,7 +57,7 @@ def add_tractography_args(app_parser: BidsAppArgumentParser) -> None:
         choices=["wm", "act"],
         help="tractography seeding method (one of [%(choices)s]; default: %(default)s)",
     )
-    tractography_args.add_argument(
+    arg_group.add_argument(
         "--cutoff",
         metavar="cutoff",
         dest="participant.tractography.cutoff",
@@ -58,7 +65,7 @@ def add_tractography_args(app_parser: BidsAppArgumentParser) -> None:
         default=0.1,
         help="cutoff FOD amplitude for track termination (default: %(default).2f)",
     )
-    tractography_args.add_argument(
+    arg_group.add_argument(
         "--streamlines",
         metavar="streamlines",
         dest="participant.tractography.streamlines",
