8be707e71718436bcdd074024b173b917f368e8b
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-02-25 09:50:01 -0500

    Gen fake t2w contrast from t1w for registration
    - Applies brain mask to the image before performing performing registration
    - Add method for selecting affine initialization in greedy
    - Use transformation on brain mask only if none is provided, otherwise just resample

--- src/nhp_dwiproc/workflow/diffusion/preprocess/registration.py
+++ src/nhp_dwiproc/workflow/diffusion/preprocess/registration.py
@@ -8,10 +8,11 @@ from functools import partial
 from logging import Logger
 from typing import Any
 
-from niwrap import ants, c3d, greedy, mrtrix
+from niwrap import ants, c3d, fsl, greedy, mrtrix
 from styxdefs import InputPathType, OutputPathType
 
 import nhp_dwiproc.utils as utils
+from nhp_dwiproc.lib.anat import fake_t2w
 from nhp_dwiproc.lib.dwi import rotate_bvec
 from nhp_dwiproc.utils.assets import load_nifti
 
@@ -36,18 +37,31 @@ def register(
         fslgrad=mrtrix.dwiextract_fslgrad_params(bvecs=bvec, bvals=bval),
         bzero=True,
     )
-
     b0 = mrtrix.mrmath(
         input_=[b0.output],
         output=bids(desc="avg", suffix="b0", ext=".nii.gz"),
         operation="mean",
         axis=3,
     )
+    b0_brain = fsl.fslmaths(
+        input_files=[b0.output],
+        operations=[fsl.fslmaths_operation_params(mas=mask)],
+        output=bids(desc="avgBrain", suffix="b0", ext=".nii.gz"),
+    )
+
+    # Fake T2w contrast for registration
+    logger.info("Generating fake T2w brain from T1w.")
+    t2w = fake_t2w(t1w=input_data["t1w"]["nii"], input_group=input_group, cfg=cfg)
+    t2w_brain = fsl.fslmaths(
+        input_files=[t2w],
+        operations=[fsl.fslmaths_operation_params(mas=input_data["dwi"].get("mask"))],
+        output=bids(desc="fakeBrain", suffix="T2w", ext=".nii.gz"),
+    )
 
     # Perform registration
-    b0_to_t1 = greedy.greedy_(
+    b0_to_t2 = greedy.greedy_(
         input_images=greedy.greedy_input_images_params(
-            fixed=input_data["t1w"]["nii"], moving=b0.output
+            fixed=t2w_brain.output_file, moving=b0_brain.output_file
         ),
         output=bids(
             datatype="anat",
@@ -60,9 +74,8 @@ def register(
         ).replace("from_", "from"),
         affine=True,
         affine_dof=6,
-        ia_identity=True,
-        fixed_mask=input_data["dwi"].get("mask"),
-        moving_mask=mask,
+        ia_identity=cfg["participant.preprocess.register.init"] == "identity",
+        ia_image_centers=cfg["participant.preprocess.register.init"] == "image-centers",
         iterations=cfg["participant.preprocess.register.iters"],
         metric=greedy.greedy_metric_params(
             cfg["participant.preprocess.register.metric"]
@@ -70,11 +83,11 @@ def register(
         dimensions=3,
         threads=cfg["opt.threads"],
     )
-    transforms = {"ras": b0_to_t1.output_file}
+    transforms = {"ras": b0_to_t2.output_file}
     if not transforms.get("ras"):
         raise ValueError("No RAS transformation found")
     b0_resliced = greedy.greedy_(
-        fixed_reslicing_image=input_data["t1w"]["nii"],
+        fixed_reslicing_image=t2w,
         reslice_moving_image=greedy.greedy_reslice_moving_image_params(
             moving=b0.output,
             output=bids(space="T1w", desc="avg", suffix="b0", ext=".nii.gz"),
@@ -158,9 +171,9 @@ def apply_transform(
         input_image_type=0,
         input_image=input_data["dwi"].get("mask") or mask,
         reference_image=ref_b0,
-        transform=[
-            ants.ants_apply_transforms_transform_file_name_params(transforms["itk"])
-        ],
+        transform=None
+        if input_data["dwi"].get("mask")
+        else [ants.ants_apply_transforms_transform_file_name_params(transforms["itk"])],
         interpolation=ants.ants_apply_transforms_nearest_neighbor_params(),
         output=ants.ants_apply_transforms_warped_output_params(
             bids(space="T1w", res="dwi", desc="preproc", suffix="mask")
