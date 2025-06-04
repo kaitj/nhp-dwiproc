79ebd5bed14edd74dedbe1955a95ec5d3fcc813f
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-06-04 11:30:47 -0400

    Update index level
    - Switches to using niwrap_helper which had similar code anyways
    - Return and save the parquet via pyarrow

--- src/nhp_dwiproc/app/analysis_levels/index.py
+++ src/nhp_dwiproc/app/analysis_levels/index.py
@@ -4,21 +4,19 @@ from logging import Logger
 from pathlib import Path
 from typing import Any
 
-from bids2table import bids2table
+import pyarrow.parquet as pq
+from niwrap_helper.bids import get_bids_table
 
 
 def run(cfg: dict[str, Any], logger: Logger) -> None:
     """Runner for index-level analysis."""
     logger.info("Index analysis-level")
-    index_path: Path = cfg.get("opt.index_path", cfg["bids_dir"] / "index.b2t")
+    index_path: Path = cfg.get("opt.index_path", cfg["bids_dir"] / ".index.b2t")
     if index_path.exists() and not cfg["index.overwrite"]:
         logger.info("Index already exists - not overwriting")
     else:
         logger.info("Indexing bids dataset...")
-        bids2table(
-            root=cfg["bids_dir"],
-            index_path=index_path,
-            overwrite=cfg["index.overwrite"],
-            persistent=True,
-            workers=cfg["opt.threads"],
+        table = get_bids_table(
+            dataset_dir=cfg["bids_dir"], index=index_path, return_type="pyarrow"
         )
+        pq.write_table(table, index_path)
