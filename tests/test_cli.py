12e5db658be0345cdad4027a857ebe795c218122
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-09-12 14:43:16 -0400

    Add testing for config + cli options
    - Remove extra print that isn't necessary

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+import yaml
 from typer.testing import CliRunner
 
 from nhp_dwiproc.cli import app
@@ -40,3 +41,102 @@ class TestCLI:
             args=[str(tmp_path), str(tmp_path), "preprocess", "--runner", "invalid"],
         )
         assert result.exit_code == 2
+
+
+class TestAppOptions:
+    """Tests for passing options via config and CLI."""
+
+    @pytest.fixture
+    def valid_cfg_yaml(self, tmp_path: Path):
+        valid_cfg = {
+            "opts": {"threads": 1, "runner": {"name": "docker"}},
+            "preprocess": {"query": {"participant": "sub=='001'"}},
+        }
+        file_path = tmp_path / "valid.yaml"
+        file_path.write_text(yaml.safe_dump(valid_cfg))
+
+        return file_path
+
+    @pytest.fixture
+    def invalid_cfg_yaml(self, tmp_path: Path):
+        valid_cfg = {
+            "opts": {"threads": 1, "runner": {"name": "docker"}},
+            "preprocess": {"query": {"participant": "sub=='001'"}},
+            "invalid": "value",
+        }
+        file_path = tmp_path / "valid.yaml"
+        file_path.write_text(yaml.safe_dump(valid_cfg))
+
+        return file_path
+
+    def test_valid_config_stage(self, tmp_path: Path, valid_cfg_yaml: Path):
+        """Test running with a valid config."""
+        result = runner.invoke(
+            app,
+            args=[
+                str(tmp_path),
+                str(tmp_path),
+                "preprocess",
+                "--config",
+                str(valid_cfg_yaml),
+            ],
+        )
+        for expected in [str(valid_cfg_yaml), "docker", "sub=='001"]:
+            assert expected in result.output
+
+    def test_valid_stage_invalid_config(self, tmp_path: Path, invalid_cfg_yaml: Path):
+        """Test running with ignored keys in invalid config."""
+        result = runner.invoke(
+            app,
+            args=[
+                str(tmp_path),
+                str(tmp_path),
+                "preprocess",
+                "--config",
+                str(invalid_cfg_yaml),
+            ],
+        )
+        assert result.exit_code == 0
+        for expected in [str(invalid_cfg_yaml), "docker", "sub=='001"]:
+            assert expected in result.output
+        assert "value" not in result.output
+
+    def test_invalid_stage_config(self, tmp_path: Path, valid_cfg_yaml: Path):
+        """Test to ensure stage specific params aren't passed to wrong stage."""
+        result = runner.invoke(
+            app,
+            args=[
+                str(tmp_path),
+                str(tmp_path),
+                "connectivity",
+                "--config",
+                str(valid_cfg_yaml),
+            ],
+        )
+        for expected in [str(valid_cfg_yaml), "docker"]:
+            assert expected in result.output
+        assert "sub=='001'" not in result.output
+
+    def test_cli_overwrite(self, tmp_path: Path, valid_cfg_yaml: Path):
+        """Test overwriting config parameters with CLI."""
+        result = runner.invoke(
+            app,
+            args=[
+                str(tmp_path),
+                str(tmp_path),
+                "preprocess",
+                "--config",
+                str(valid_cfg_yaml),
+                "--runner",
+                "singularity",
+                "--threads",
+                "4",
+            ],
+        )
+        for expected in [
+            str(valid_cfg_yaml),
+            "sub=='001",
+            "singularity",
+            "threads=4",
+        ]:
+            assert expected in result.output
