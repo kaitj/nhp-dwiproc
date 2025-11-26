ed48899e271f605be0fa744104892297bbb298fa
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-11-26 15:49:23 -0500

    Splt up CLI (#105)
    - Split typer commands to own submodules
      - Moved off for easier maintenance long-term
      - Added utility functions for commonly used snippets
    - Add tests for new / updated public methods
    - Add test using mock to cover missing line

--- src/nhp_dwiproc/cli/utils.py
+++ src/nhp_dwiproc/cli/utils.py
@@ -1,14 +1,67 @@
 """Utility functions specifically for working in the CLI."""
 
 import json
+import logging
+from functools import partial
 from pathlib import Path
+from types import SimpleNamespace
 from typing import Any
 
 import typer
 import yaml
 
+from nhp_dwiproc import config as cfg
 
-def _json_dict_callback(value: str | None) -> dict[str, str] | None:
+LOG_LEVELS = [logging.INFO, logging.DEBUG]
+
+
+def setup_logging(verbose: int) -> int:
+    """Convert verbose count to logging level."""
+    return (
+        LOG_LEVELS[min(verbose, len(LOG_LEVELS)) - 1]
+        if verbose > 0
+        else logging.CRITICAL + 1
+    )
+
+
+def build_global_opts(
+    ctx_params: dict,
+    cfg_file: Path | None,
+    prefix: str = "opts_",
+) -> cfg.GlobalOptsConfig:
+    """Build global options configuration."""
+    local_vars = {k: v for k, v in ctx_params.items() if k.startswith(prefix)}
+    mapper = partial(cfg.utils.map_param, vars_dict=local_vars)
+
+    opt_map = mapper(prefix, "")
+    opt_map.update(
+        {f"{prefix}runner": "runner.name", f"{prefix}images": "runner.images"}
+    )
+    builder = partial(cfg.utils.build_config, ctx_params=ctx_params, cfg_file=cfg_file)
+    return builder(
+        cfg_class=cfg.GlobalOptsConfig,
+        cfg_key="opts",
+        include_only=list(opt_map.keys()),
+        cli_map=opt_map,
+    )
+
+
+def finalize_stage(
+    ctx: SimpleNamespace, logger: logging.Logger, include_descriptor: bool = True
+) -> None:
+    """Finalization steps for each stage."""
+    logger.setLevel(ctx.log_level)
+    logger.debug(f"Stage options:\n\n{_namespace_to_yaml(obj=ctx)}")
+
+    if include_descriptor:
+        cfg.utils.generate_descriptor(
+            app_name=ctx.app,
+            version=ctx.version,
+            out_fpath=ctx.cfg.output_dir / "dataset_description.json",
+        )
+
+
+def json_dict_callback(value: str | None) -> dict[str, str] | None:
     """Callback helper to convert CLI images to dictionary.
 
     Args:
